dfb8fde04401f2cdc989bb5bfa612f783240d15e
Kalpan Jasani <kjasani@purdue.edu>
2018-05-17 16:07:40 -0400

    have addition of list items in the page/dom

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 //the main form
 const entryForm = document.getElementById("entryForm");
+
+//adding event listener on the form
 entryForm.addEventListener("submit", onSubmit)
-function Entry(name, version, android_version, domain)
+
+//constructor for user entry objects
+function Entry(name, version, android_version, domain, link)
 {
     this.name = name;
     this.version = version;
     this.android_version = android_version;
+    this.domain = domain;
+    this.link = link;
 }
 
+//array for storing entries
+const entries = [];
+
+//event listener for submit event on the form
 function onSubmit(ev)
 {
     ev.preventDefault();
@@ -25,18 +35,20 @@ function onSubmit(ev)
         spanText.style.display = "none";
     }
 
-    const entry = new Entry(entryForm.name.value, entryForm.version.value, entryForm.android_version.value, entryForm.domain.value);
+    const entry = new Entry(entryForm.name.value, entryForm.version.value, entryForm.android_version.value, entryForm.domain.value, entryForm.link.value);
+    entries.push(entry);
     const deleteButton = document.createElement("BUTTON");
     deleteButton.innerHTML = "del";
     deleteButton.style.display = "none";
     deleteButton.style.float = "right";
+
+    //we don't account for button to grow bigger than the size of its list item container, as that will only happen on a mouseover event
     
-    //creating a text node for the data payload
-    const textNode = document.createTextNode(`${entry.name}`);
+    
 
     //creating a list item
     const listItem = document.createElement("LI");
-    listItem.appendChild(textNode);
+    listItem.appendChild(createDetail(entry));
     listItem.appendChild(deleteButton);
 
     listItem.addEventListener("mouseover", function()
@@ -54,3 +66,30 @@ function onSubmit(ev)
 
 }
 
+
+function createDetail(entry)
+{
+    const textSpan = document.createElement("SPAN");
+    textSpan.innerHTML = "<b>" + entry.name + ": " + "</b>";
+
+    if(entry.version != "")
+        textSpan.innerHTML += "ver " + entry.version + ", ";
+    if(entry.value == "")
+        textSpan.innerHTML += "android ver " + entry.android_version + ", ";
+    
+    if(entry.value != "")
+        textSpan.innerHTML += "domain " + entry.domain + ", ";
+    
+    if(entry.value != "")
+    {
+        //create an a tag
+        const link = document.createElement("A");
+        link.href = entry.link;
+        link.innerText = "link";
+        
+        //adding anchor to span
+        textSpan.appendChild(link);
+    }
+
+    return textSpan;
+}
